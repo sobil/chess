d8ec39676310b7f467fe0c72b8497310f722ec54
Simon Pilepich <simon.pilepich@kogan.com.au>
2023-12-20 01:51:39 +1100

    a simple react chess board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState } from 'react';
+
+const SquareBox = (props) => {
+  return (
+    <div className="square" style={{ backgroundColor: props.color }}>{props.text}</div>
+  );
+}
+
+const ChessBoard = (layout = [], clickAction, selectedPiece) => {
+  return layout.map((row, i) => {
+    return (
+      <tr key={i}>
+        {row.map((cell, j) => {
+          const square = i * row.length + j;
+          const defaultColor = (i + j) % 2 === 0 ? "black" : "grey";
+          const color = selectedPiece === square ? "red" : defaultColor;
+          return (
+            <td key={square} onClick={()=>clickAction(square)}>
+              <SquareBox color={color} text={cell} squareNumber={square} />
+            </td>
+          )
+        })}
+      </tr>
+    )
+
+  })
+}
+
+const App = () => {
+  let [chessBoard, updateChessBoard] = useState(
+    [
+      ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
+      ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+      new Array(8).fill(""),
+      new Array(8).fill(""),
+      new Array(8).fill(""),
+      new Array(8).fill(""),
+      ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+      ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
+    ])
+  let [selectedPiece, selectSquare] = useState(-1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tbody>
+            {ChessBoard(chessBoard,selectSquare,selectedPiece,updateChessBoard)}
+          </tbody>
+        </table>
+
       </header>
     </div>
   );
 }
+
+
 
 export default App;
